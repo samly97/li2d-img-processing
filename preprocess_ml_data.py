b2958e5075a74dbe6e7a45890485812964bfa831
samly97 <97.sam.ly@gmail.com>
2022-02-25 12:22:13 -0500

    MCP-27 Refactored into ETL functionality into utils, importing from utils
    - Checked that it can run separately on Jupyter Labs
    - Still need to validate it (see notes on JIRA)
    
    TODO:
    - Refactor utils/ml.py to use new ETL utils

--- preprocess_ml_data.py
+++ preprocess_ml_data.py
@@ -1,48 +1,51 @@
 import os
-from typing import List, Tuple
-from math import ceil
+from typing import Dict, List, Tuple
+
 import numpy as np
 from random import shuffle
+from math import ceil
 import tensorflow as tf
 
 from utils.io import load_json
-from utils.encoder_colormap import colormap as encoder_colormap
-from utils.image import circle_mask_as_vector_of_indices
+from utils.etl import ETL_Functions, ETL_2D
+from utils.etl import leave_first, ignore_key, ignore_tensor
+
+from utils import typings
 
 
 def parse_raw_data(
     data_dir: str,
     input_dir: str,
-) -> List[str]:
+) -> List[int]:
 
     data_dir = os.path.join(os.getcwd(), data_dir)
     input_im_dir = os.path.join(data_dir, input_dir)
     input_im_fname = np.array([fname for fname in os.listdir(input_im_dir)])
 
-    pic_num = [fname.split(".png")[0] for fname in input_im_fname]
+    pic_num = [int(fname.split(".npy")[0]) for fname in input_im_fname]
 
     return pic_num
 
 
 def shuffle_dataset(
-    pic_num: List[str],
-) -> np.array:
+    pic_num: List[int],
+) -> List[int]:
 
     # Shuffle list
     list_idx = list(range(len(pic_num)))
     shuffle(list_idx)
 
     # Shuffle particle number list
-    pic_num = np.array(pic_num)
-    pic_num = pic_num[list_idx]
+    pic_num_arr = np.array(pic_num)
+    pic_num_arr = pic_num_arr[list_idx]
 
-    return pic_num
+    return pic_num_arr.tolist()
 
 
 def get_split_indices(
     trn_split: int,
     val_split: int,
-    pic_num: np.array,
+    pic_num: List[int],
 ) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
 
     trn_idx = ceil(len(pic_num) * trn_split)
@@ -50,252 +53,6 @@ def get_split_indices(
     test_idx = len(pic_num)
 
     return ((0, trn_idx), (trn_idx, val_idx), (val_idx, test_idx))
-
-
-def get_ml_dataset(
-    pic_num: np.array,
-    dataset_json,
-    width_wrt_radius: float,
-    im_size: int,
-    batch_size: int,
-    norm_metadata: Tuple[int, int, int, int, int],
-    dirs: Tuple[str, str, str],
-    start_idx: int,
-    end_idx: int,
-    scale: int = 10,
-):
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    data_dir, input_dir, label_dir = dirs
-
-    # Maximum grayscale value from RGB [0,255] to gray is 100
-    _grayscale_norm = tf.cast(
-        100,
-        dtype=tf.float32,
-    )
-
-    # Colormap to invert RGB to SOL
-    cmap = encoder_colormap()
-
-    new_data = _get_static_hash_table(
-        dataset_json,
-        val_fn=lambda num: format_metadata(
-            num,
-            dataset_json,
-            norm_metadata,
-        ),
-        default_value="",
-    )
-
-    mask = circle_mask_as_vector_of_indices(
-        width_wrt_radius,
-        im_size,
-        scale,
-    )
-
-    fname_ds = tf.data.Dataset.from_tensor_slices(pic_num[start_idx: end_idx])
-
-    def process_path_inputs(pic_num):
-        input_im = _load_image(
-            pic_num,
-            data_dir,
-            input_dir,
-            im_size,
-        )
-        input_im = tf.image.rgb_to_grayscale(input_im)
-        input_im = tf.math.divide(
-            input_im,
-            _grayscale_norm,
-        )
-
-        metadata = _format_metadata(
-            pic_num,
-            new_data,
-        )
-
-        return input_im, metadata
-
-    def process_path_targets(pic_num):
-        label_im = _load_image(
-            pic_num,
-            data_dir,
-            label_dir,
-            im_size,
-        )
-
-        sol_updates = _rgb_to_sol(
-            label_im,
-            mask,
-            cmap,
-        )
-        gray_label_im = tf.image.rgb_to_grayscale(label_im)
-        gray_label_im = tf.math.divide(
-            gray_label_im,
-            _grayscale_norm,
-        )
-        gray_label_im = _assign_sol_to_grayscale(
-            gray_label_im,
-            sol_updates,
-            mask,
-        )
-
-        return gray_label_im
-
-    def configure_for_performance(ds):
-        # https://www.tensorflow.org/tutorials/load_data/images#using_tfdata_for_finer_control
-        ds = ds.cache()
-        ds = ds.batch(batch_size)
-        ds = ds.prefetch(buffer_size=AUTOTUNE)
-        return ds
-
-    inp_ds = fname_ds.map(process_path_inputs, num_parallel_calls=AUTOTUNE)
-    out_ds = fname_ds.map(process_path_targets, num_parallel_calls=AUTOTUNE)
-
-    inp_ds = configure_for_performance(inp_ds)
-    out_ds = configure_for_performance(out_ds)
-
-    ret_ds = tf.data.Dataset.zip((inp_ds, out_ds))
-    return ret_ds
-
-
-def _get_static_hash_table(
-        hash: dict,
-        val_fn=lambda v: v,
-        default_value="") -> tf.lookup.StaticHashTable:
-    r''' The default value depends on what the data type of the key is.
-    '''
-
-    keys = [None for _ in range(0, len(hash))]
-    vals = [None for _ in range(0, len(hash))]
-
-    for i, key in enumerate(hash.keys()):
-        val = val_fn(key)
-
-        keys[i] = str(key)
-        vals[i] = val
-
-    keys = tf.constant(keys)
-    vals = tf.constant(vals)
-
-    ret_table = tf.lookup.StaticHashTable(
-        tf.lookup.KeyValueTensorInitializer(keys, vals),
-        default_value=default_value)
-
-    return ret_table
-
-
-def format_metadata(
-        pic_num: str,
-        dataset_json,
-        norm_metadata: Tuple[int, int, int, int, int]) -> List[str]:
-    r''' format_metadata is the step prior to processing data into a
-    tf.data.Dataset used for the Machine Learning pipeline. Due to TensorFlow
-    not behaving well with Dictionaries or Lists in their built-in lookup
-    tables, this step returns the metadata as strings. In the pipeline step,
-    the strings are split and parsed into floats.
-    '''
-
-    L, h_cell, R_max, zoom_norm, c_rate_norm, time_norm = norm_metadata
-
-    hash = dataset_json[pic_num]
-
-    x = float(hash["x"]) / L
-    y = float(hash["y"]) / h_cell
-    R = float(hash["R"]) / R_max
-    zoom = float(hash["zoom_factor"]) / zoom_norm
-    c_rate = float(hash["c-rate"]) / c_rate_norm
-    time = float(hash["time"]) / time_norm
-    porosity = float(hash["porosity"])
-    dist_from_sep = float(hash["dist_from_sep"])
-
-    as_float = [x, y, R, zoom, c_rate, time, porosity, dist_from_sep]
-
-    s = "-"
-    ret = s.join([str(num) for num in as_float])
-    return ret
-
-
-def _load_image(
-    file_num,
-    data_dir: str,
-    img_dir: str,
-    im_size: int,
-):
-    path = tf.strings.join([os.getcwd(), "/", data_dir, "/", img_dir])
-
-    img = tf.io.read_file(
-        tf.strings.join([path, "/", file_num, ".png"])
-    )
-    img = tf.io.decode_png(img, channels=3)
-    img = tf.image.resize(img, (im_size, im_size))
-    return img
-
-
-def _rgb_to_sol(
-    im: tf.Tensor,
-    indices: tf.Tensor,
-    cmap: encoder_colormap,
-) -> tf.Tensor:
-    r''' _rgb_to_sol "inverses" the RGB encoding as defined in
-    `utils.encoder_colormap` to State-of-Lithiation.
-
-    This function is used within a the tf.data.Dataset pipeline.
-
-    Inputs:
-    - indices: tf.Tensor(dtype=tf.int32); index locations of the particles
-        location as a reshaped vector.
-
-    Returns:
-    - sol: tf.Tensor(dtype=tf.float32); State-of-Lithiation corresponding to
-        `indices`.
-    '''
-
-    sol = cmap.inverse(im)
-    sol = tf.gather(sol, indices, axis=0)
-
-    return sol
-
-
-def _assign_sol_to_grayscale(
-    im,
-    sol,
-    indices
-):
-    r''' _assign_sol_to_grayscale essentially works like array indexing, but
-    we require a TensorFlow function, `tf.tensor_scatter_nd_update`, to handle
-    this for us.
-
-    This sets the State-of-Lithiation located in the particle of interest
-    located by `indices`, which is a reshaped vector. Then, the vector is
-    reshaped back into a grayscale image.
-    '''
-
-    Y, X, _ = im.shape
-    dim = tf.math.multiply(Y, X)
-
-    # Reshape indices to work with the scatter_update
-    indices = tf.expand_dims(indices, axis=-1)
-
-    reshaped_im = tf.reshape(im, (dim, 1))
-    reshaped_im = tf.tensor_scatter_nd_update(
-        reshaped_im,
-        indices,
-        sol
-    )
-
-    # Convert from vector to image array
-    im = tf.reshape(reshaped_im, (Y, X, 1))
-    return im
-
-
-def _format_metadata(
-    pic_num: tf.Tensor,
-    metadata_tensor: tf.lookup.StaticHashTable,
-):
-    as_str = metadata_tensor[pic_num]
-    str_nums = tf.strings.split(as_str, sep="-")
-    ret = tf.strings.to_number(str_nums, out_type=tf.dtypes.float32)
-    return ret
 
 
 def preprocess_ml_data() -> Tuple[
@@ -307,16 +64,16 @@ def preprocess_ml_data() -> Tuple[
     '''
     settings = load_json("pipeline.json")
 
-    norm_metadata = (
-        settings["L"],
-        settings["h_cell"],
-        settings["R_max"],
-        settings["zoom_max"],
-        settings["c-rate"],
-        settings["time"],
-    )
+    norm_metadata: typings.Metadata_Normalizations = {
+        "L": settings["L"],
+        "h_cell": settings["h_cell"],
+        "R_max": settings["R_max"],
+        "zoom_norm": settings["zoom_max"],
+        "c_rate_norm": settings["c-rate"],
+        "time_norm": settings["time"],
+    }
 
-    dataset_json = load_json(
+    dataset_json: Dict[str, typings.Metadata] = load_json(
         settings["dataset_data"],
         path=os.path.join(os.getcwd(), settings["data_dir"]),
     )
@@ -335,23 +92,49 @@ def preprocess_ml_data() -> Tuple[
 
     datasets = [None, None, None]
 
+    process_input_fns: List[typings.ETL_fn] = [
+        ignore_tensor(leave_first(
+            ETL_Functions.load_npy_arr_from_dir,
+            settings["data_dir"],
+            settings["input_dir"],
+            settings["img_size"],
+            settings["tf_img_size"],
+            0,
+        )),
+        ignore_key(leave_first(
+            tf.math.divide,
+            tf.cast(2 ** 16 - 1, dtype=tf.float32),
+        )),
+    ]
+
+    process_target_fns = [
+        ignore_tensor(leave_first(
+            ETL_Functions.load_npy_arr_from_dir,
+            settings["data_dir"],
+            settings["label_dir"],
+            settings["img_size"],
+            settings["tf_img_size"],
+            0,
+        )),
+        ignore_key(leave_first(
+            tf.math.divide,
+            tf.cast(60000, dtype=tf.float32)
+        )),
+    ]
+
     for idx, tup in enumerate([trn_idx, val_idx, test_idx]):
         start_idx, end_idx = tup
+        criteria_arr = pic_num[start_idx:end_idx]
 
-        datasets[idx] = get_ml_dataset(
-            pic_num,
+        etl = ETL_2D(
             dataset_json,
-            settings["width_wrt_radius"],
-            settings["im_size"],
-            settings["batch_size"],
             norm_metadata,
-            (settings["data_dir"],
-             settings["input_dir"],
-             settings["label_dir"]),
-            start_idx=start_idx,
-            end_idx=end_idx,
-            scale=settings["scale"],
+            criteria_arr,
+            settings["batch_size"],
+            process_input_fns,
+            process_target_fns,
         )
+        datasets[idx] = etl.get_ml_dataset()
 
     trn_dataset = datasets[0]
     val_dataset = datasets[1]
---
+++ utils/etl.py
@@ -0,0 +1,234 @@
+
+import os
+from typing import List, Dict, Tuple, Callable, Union
+
+import tensorflow as tf
+import numpy as np
+from scipy.ndimage import zoom
+
+from utils import typings
+
+
+class ETL_Functions():
+
+    @staticmethod
+    def load_npy_arr_from_dir(
+        pic_num: int,
+        data_dir: str,
+        img_dir: str,
+        img_size: int,
+        tf_img_size: int,
+        order: int = 0,
+    ) -> tf.types.experimental.TensorLike:
+        path = tf.strings.join(
+            [os.getcwd(), "/", data_dir, "/", img_dir]
+        )
+        fname = tf.strings.join(
+            [path, "/", tf.strings.as_string(pic_num), ".npy"])
+
+        img = tf.numpy_function(
+            np.load, [fname], tf.uint16,
+        )
+        img = tf.cast(img, dtype=tf.float32)
+
+        zoom_factor = tf_img_size / img_size
+        img = tf.py_function(
+            lambda a: zoom(a, (zoom_factor, zoom_factor, 1), order=order),
+            [img],
+            tf.float32,
+        )
+        return img
+
+    @ staticmethod
+    def gather_img_from_np_arr(
+        idx_num: int,
+        arr_imgs: np.ndarray,
+        tf_img_size: int,
+    ) -> tf.types.experimental.TensorLike:
+        "Note: may not work, or need to change to use scipy.zoom"
+        img = tf.gather(arr_imgs, idx_num)
+        img = tf.convert_to_tensor(img)
+        img = tf.image.resize(img, (tf_img_size, tf_img_size))
+        return img
+
+    @ staticmethod
+    def format_metadata(
+        idx_num: int,
+        metadata_lookup_table: tf.lookup.StaticHashTable,
+    ) -> tf.types.experimental.TensorLike:
+        key = tf.strings.as_string(idx_num)
+        as_str = metadata_lookup_table[key]
+        str_nums = tf.strings.split(as_str, sep="-")
+        ret = tf.strings.to_number(str_nums, out_type=tf.dtypes.float32)
+        return ret
+
+    @ staticmethod
+    def fake_output(
+        idx_num: int,
+        tf_img_size: int,
+    ) -> tf.types.experimental.TensorLike:
+        "Note: may not work... may need to wrap it in tf.numpy_fn"
+        _ = idx_num
+        blank_im = np.zeros(
+            (tf_img_size, tf_img_size, 1)
+        )
+        blank_im = tf.convert_to_tensor(blank_im)
+        return blank_im
+
+
+class ETL_2D():
+
+    def __init__(
+        self,
+        metadata: Dict[str, typings.Metadata],
+        metadata_norm: typings.Metadata_Normalizations,
+        criteria_arr: List[int],
+        batch_size: int,
+        process_input_fns: List[typings.ETL_fn],
+        process_target_fns: List[typings.ETL_fn],
+    ):
+        self.AUTOTUNE = tf.data.AUTOTUNE
+        self.metadata_norm = metadata_norm
+        self.batch_size = batch_size
+
+        self.metadata_lookup = self.get_static_hash_table(
+            metadata,
+            process_value_fn=lambda num: self._format_metadata(
+                num,
+                metadata,
+            ),
+            default_value="",
+        )
+
+        self.starting_ds = tf.data.Dataset.from_tensor_slices(criteria_arr)
+
+        self.input_fns = process_input_fns
+        self.output_fns = process_target_fns
+
+    def get_ml_dataset(self) -> tf.data.Dataset:
+
+        def configure_for_performance(ds):
+            # https://www.tensorflow.org/tutorials/load_data/images#using_tfdata_for_finer_control
+            ds = ds.cache()
+            ds = ds.batch(self.batch_size)
+            ds = ds.prefetch(buffer_size=self.AUTOTUNE)
+            return ds
+
+        inp_ds = self.starting_ds.map(
+            self._process_input_path, num_parallel_calls=self.AUTOTUNE)
+        out_ds = self.starting_ds.map(
+            self._process_output_path, num_parallel_calls=self.AUTOTUNE)
+
+        inp_ds = configure_for_performance(inp_ds)
+        out_ds = configure_for_performance(out_ds)
+
+        ret_ds = tf.data.Dataset.zip((inp_ds, out_ds))
+        return ret_ds
+
+    def _process_input_path(
+        self,
+        arr_idx: int,
+        input_im: tf.types.experimental.TensorLike = None,
+    ) -> Tuple[
+            tf.types.experimental.TensorLike,
+            tf.types.experimental.TensorLike,
+    ]:
+        for fn in self.input_fns:
+            input_im = fn(arr_idx, input_im)
+
+        metadata = ETL_Functions.format_metadata(
+            arr_idx,
+            self.metadata_lookup,
+        )
+
+        return input_im, metadata
+
+    def _process_output_path(
+        self,
+        arr_idx: int,
+        target_im: tf.types.experimental.TensorLike = None,
+    ) -> tf.types.experimental.TensorLike:
+        for fn in self.output_fns:
+            target_im = fn(arr_idx, target_im)
+
+        return target_im
+
+    def get_static_hash_table(
+        self,
+        hash,
+        process_value_fn: Callable[[int], str],
+        default_value: str = "",
+    ) -> tf.lookup.StaticHashTable:
+        r''' The default value depends on what the data type of the key is.
+        '''
+        keys: List[Union[str, None]] = [None for _ in range(0, len(hash))]
+        vals: List[Union[str, None]] = [None for _ in range(0, len(hash))]
+
+        for i, key in enumerate(hash.keys()):
+            val = process_value_fn(key)
+
+            keys[i] = str(key)
+            vals[i] = val
+
+        keys = tf.constant(keys)
+        vals = tf.constant(vals)
+
+        ret_table = tf.lookup.StaticHashTable(
+            tf.lookup.KeyValueTensorInitializer(keys, vals),
+            default_value=default_value,
+        )
+
+        return ret_table
+
+    def _format_metadata(
+        self,
+        pic_num: int,
+        metadata: Dict[str, typings.Metadata],
+    ) -> str:
+
+        hash = metadata[str(pic_num)]
+
+        x = float(hash["x"]) / self.metadata_norm["L"]
+        y = float(hash["y"]) / self.metadata_norm["h_cell"]
+        R = float(hash["R"]) / self.metadata_norm["R_max"]
+        zoom = float(hash["zoom_factor"]) / self.metadata_norm["zoom_norm"]
+        c_rate = float(hash["c_rate"]) / self.metadata_norm["c_rate_norm"]
+        time = float(hash["time"]) / self.metadata_norm["time_norm"]
+        porosity = float(hash["porosity"])
+        dist_from_sep = float(hash["dist_from_sep"])
+
+        as_float = [x, y, R, zoom, c_rate, time, porosity, dist_from_sep]
+
+        s = "-"
+        ret = s.join(str(num) for num in as_float)
+        return ret
+
+###############################################################################
+# DECORATORS ##################################################################
+###############################################################################
+
+
+def leave_first(fn, *args):
+    def wrapped(
+        first_arg: int,
+    ) -> typings.ETL_key_fn:
+        return fn(first_arg, *args)
+    return wrapped
+
+
+def ignore_key(
+    fn: typings.ETL_key_fn,
+) -> typings.ETL_key_and_tensor_fn:
+    def wrapped(arr_idx, tensor):
+        _ = arr_idx
+        return fn(tensor)
+    return wrapped
+
+
+def ignore_tensor(
+    fn: typings.ETL_key_fn
+) -> typings.ETL_key_and_tensor_fn:
+    def wrapped(arr_idx, tensor):
+        _ = tensor
+        return fn(arr_idx)
+    return wrapped
