2b1c2f040c22e23cb50abcc06d607e116e18c1ed
samly97 <97.sam.ly@gmail.com>
2022-03-20 15:57:02 -0400

    MCP-30 Implemented local tortuosity functionality
    - extracted image up to the center of the particle-of-interest then measures
      the tortuosity in this way

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,9 @@
+from utils import typings
+
+from math import ceil
+
 import numpy as np
+import porespy as ps
 
 
 def measure_porosity(
@@ -30,3 +35,47 @@ def measure_porosity(
     porosity = pore_space / electrode_domain
 
     return porosity
+
+
+def tortuosity_to_particle(
+    micro_im: np.ndarray,
+    particle: typings.Circle_Info,
+    width_wrt_radius: int,
+    pore_encoding: int,
+    scale: int = 10,
+) -> float:
+
+    micro_im = np.copy(micro_im)
+    Y_max = np.shape(micro_im)[1]
+
+    # Location of particle centroids in pixel-scale
+    x = float(particle["x"]) * scale
+    y = float(particle["y"]) * scale
+
+    x = ceil(x) - 1
+    y = ceil(y) - 1
+
+    # Get how much region to extract in the through-plane direction
+    R = float(particle["R"])
+    through_region = ceil(R * scale * width_wrt_radius)
+
+    # - On how much in the through (y-dir) to extract
+    # - Deal with edge cases
+    t_idx_up = (y - through_region
+                if y - through_region >= 0 else 0)
+    t_idx_down = (y + through_region
+                  if y + through_region <= Y_max - 1 else Y_max - 1)
+
+    # Small region to calculate tortuosity from
+    extracted_im = micro_im[t_idx_up:t_idx_down, :x, :]
+    extracted_im = np.reshape(extracted_im, np.shape(extracted_im)[:-1])
+    pores = extracted_im == pore_encoding
+
+    extracted_im = np.zeros(np.shape(extracted_im), dtype=bool)
+    extracted_im[pores] = True
+
+    # Simulation along the in-plane (x-direction)
+    sim = ps.dns.tortuosity(extracted_im, axis=0)
+    tortuosity = sim.tortuosity
+
+    return tortuosity
