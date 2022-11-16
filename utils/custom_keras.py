8c32896857a0aecbf207f3031e050a299dcb5f73
samly97 <97.sam.ly@gmail.com>
2022-11-16 14:00:20 -0500

    MCP-46 Initial commit of 'dual mask' loss

--- utils/custom_keras.py
+++ utils/custom_keras.py
@@ -3,6 +3,27 @@ import tensorflow as tf
 from .image import circle_mask_as_vector_of_indices
 
 
+class Mask_MSE(tf.keras.losses.Loss):
+
+    def __init__(self, name="mask_mask"):
+        super().__init__(name=name)
+
+    def call(self, y_true, y_pred):
+        # Evaluate pixels on the intersection where `y_pred` and `y_true` have
+        # valid input
+        mask_pred = tf.math.greater(y_pred, 0.0)
+        mask_true = tf.math.greater(y_true, 0.0)
+
+        # Intersection between both masks
+        mask = tf.math.logical_and(mask_pred, mask_true)
+        mask = tf.cast(mask, tf.float32)
+
+        y_pred = tf.math.multiply(y_pred, mask)
+        y_true = tf.math.multiply(y_true, mask)
+
+        return tf.math.reduce_mean(tf.square(y_pred - y_true))
+
+
 class Custom_2D_ROI_MSE(tf.keras.losses.Loss):
 
     def __init__(
