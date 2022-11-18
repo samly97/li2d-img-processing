eb4f1d54c79ac46d915586443f42403947192539
samly97 <97.sam.ly@gmail.com>
2022-11-18 16:50:43 -0500

    MCP-46 Small modification to finding the mask for `y_pred`
    Look for negative and positive values because the Neural Net could output
    negative values, and the way we had the mask before it would not count
    those values in the mask even though they are valid values.

--- utils/custom_keras.py
+++ utils/custom_keras.py
@@ -11,7 +11,14 @@ class Mask_MSE(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
         # Evaluate pixels on the intersection where `y_pred` and `y_true` have
         # valid input
-        mask_pred = tf.math.greater(y_pred, 0.0)
+
+        # Just making sure we're not ignoring negatives, since if the
+        # predictions are negative within the valid region then we're
+        # artificially masking out these regions.
+        mask_pred_greater = tf.math.greater(y_pred, 0.0)
+        mask_pred_lesser = tf.math.less(y_pred, 0.0)
+
+        mask_pred = tf.math.logical_or(mask_pred_greater, mask_pred_lesser)
         mask_true = tf.math.greater(y_true, 0.0)
 
         # Intersection between both masks
